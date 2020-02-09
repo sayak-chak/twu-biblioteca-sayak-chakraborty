2da8959bee756cf0298afb57e0bf06880fcb0eca
Sayak Chakraborty <sayak.chakraborty@thoughtworks.com>
2020-02-09 12:23:56 +0530

    Requirement 1.3 | Book class can display the name, author and year of publication of the book

--- src/test/java/com/twu/biblioteca/BookTest.java
+++ src/test/java/com/twu/biblioteca/BookTest.java
@@ -16,12 +16,23 @@ class BookTest {
         outContent = new ByteArrayOutputStream();
         System.setOut(new PrintStream(outContent));
     }
+
     @Test
     void testShouldDisplayTheBookName() {
-        Book book = new Book(ADUMMYBOOK.NAME);
+        Book book = new Book(ADUMMYBOOK.NAME, ADUMMYBOOK.AUTHOR, ADUMMYBOOK.yearOfPublication);
         String expectedOutput = ADUMMYBOOK.NAME +"\n";
 
         book.getBookName();
+
+        assertEquals(expectedOutput, outContent.toString());
+    }
+
+    @Test
+    void testShouldDisplayTheBookNameAuthorAndYearOfPublication() {
+        Book book = new Book(ADUMMYBOOK.NAME, ADUMMYBOOK.AUTHOR, ADUMMYBOOK.yearOfPublication);
+        String expectedOutput = ADUMMYBOOK.NAME +" | "+ ADUMMYBOOK.AUTHOR +" | "+ ADUMMYBOOK.yearOfPublication+"\n";
+        book.getBookAuthorAndYearOfPublication();
+
 
         assertEquals(expectedOutput, outContent.toString());
     }
