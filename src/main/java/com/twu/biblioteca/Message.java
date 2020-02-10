7e6e2945958223978fb364e3e316b6abdc80ea97
Sayak Chakraborty <sayak.chakraborty@thoughtworks.com>
2020-02-10 15:37:09 +0530

    Requirement 1.4 | Fix | Included menu instructions

--- src/main/java/com/twu/biblioteca/Message.java
+++ src/main/java/com/twu/biblioteca/Message.java
@@ -6,6 +6,7 @@ public final class Message {
     static final String checkOutFail = "Sorry, that book is not available";
     static final String successfulCheckOut = "Thank You! Enjoy the book.";
     static final String invalidOption = "Please select a valid option!";
+    public static String instruction = "Please choose an option";
     static final String menuOptionOne = "List Of Books";
     static final String menuOptionFour = "Quit";
     public static String menuOptionTwo = "Checkout a book";
