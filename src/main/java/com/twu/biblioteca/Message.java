55f5500761d9cab419a48a7bdf04c364beedb751
Sayak Chakraborty <sayak.chakraborty@thoughtworks.com>
2020-02-10 22:31:00 +0530

    Requirement 1.1 - 1.12 | Refactor | Made the Message class & all its data members package private

--- src/main/java/com/twu/biblioteca/Message.java
+++ src/main/java/com/twu/biblioteca/Message.java
@@ -1,17 +1,16 @@
 package com.twu.biblioteca;
 
-//TODO: package private
-public final class Message {
+final class Message {
     static final String returnFail = "That is not a valid book to return.";
     static final String successfulReturn = "Thank you for returning the book";
     static final String checkOutFail = "Sorry, that book is not available";
     static final String successfulCheckOut = "Thank You! Enjoy the book.";
     static final String invalidOption = "Please select a valid option!";
-    public static String instruction = "Please choose an option";
+    static final String instruction = "Please choose an option";
     static final String menuOptionOne = "List Of Books";
     static final String menuOptionFour = "Quit";
-    public static String menuOptionTwo = "Checkout a book";
-    public static String menuOptionThree = "Return a book";
-    static String quit = "Bye!";
-    static String welcome = "Welcome to Biblioteca. Your one-stop-shop for great book titles in Bangalore!";
+    static final String menuOptionTwo = "Checkout a book";
+    static final String menuOptionThree = "Return a book";
+    static final String quit = "Bye!";
+    static final String welcome = "Welcome to Biblioteca. Your one-stop-shop for great book titles in Bangalore!";
 }
