c960eeda5014d6b4d4f345e5dc57d4fc2383ba85
Sayak Chakraborty <sayak.chakraborty@thoughtworks.com>
2020-02-12 15:10:09 +0530

    Requirement 2.1 | Add | Movie class extending Item interface | Extract | Functionalities from Book class to Movie class

--- src/main/java/com/twu/biblioteca/Movie.java
+++ src/main/java/com/twu/biblioteca/Movie.java
@@ -1,5 +1,23 @@
 package com.twu.biblioteca;
 
-public abstract class Movie implements Item {
+public class Movie implements Item {
+    private String name, director;
+    private int yearOfPublication, rating;
 
+    Movie(String name, String director, int yearOfPublication, int rating) {
+        this.name = name;
+        this.director = director;
+        this.yearOfPublication = yearOfPublication;
+        this.rating = rating;
+    }
+
+    @Override
+    public String getName() {
+        return this.name;
+    }
+
+    @Override
+    public String getDetails() {
+        return this.name + " | " + this.director + " | " + this.yearOfPublication + " | " + this.rating;
+    }
 }
--- src/test/java/com/twu/biblioteca/ExpectedTestOutputs.java
+++ src/test/java/com/twu/biblioteca/ExpectedTestOutputs.java
@@ -2,6 +2,10 @@ package com.twu.biblioteca;
 
 public class ExpectedTestOutputs {
     static final String bookList = "1. " + DummyBooks.bookOneName + " | " + DummyBooks.bookOneAuthor + " | " + DummyBooks.bookOneYearOfPublication + "\n" + "2. " + DummyBooks.bookTwoName + " | " + DummyBooks.bookTwoAuthor + " | " + DummyBooks.bookTwoYearOfPublication + "\n";
+
     static final String menu = "1. List Of Books\n2. Checkout a book\n3. Return a book\n4. Quit\n";
+
     static final String bookOneDetails = DummyBooks.bookOneName + " | " + DummyBooks.bookOneAuthor + " | " + DummyBooks.bookOneYearOfPublication + "\n";
+
+    static final String movieOneDetails = DummyMovies.movieOneName + " | " + DummyMovies.movieOneDirector + " | " + DummyMovies.movieOneYearOfPublication + " | " + DummyMovies.movieOneRating + "\n";
 }
