9152d9efccc6f6dadf4fcaecb6fe1a02c17c4283
Sayak Chakraborty <sayak.chakraborty@thoughtworks.com>
2020-02-12 20:06:35 +0530

    Requirement 2.4 | Fix | validUser() method of Authenticator class prints user information on successful validation

--- src/main/java/com/twu/biblioteca/Authenticator.java
+++ src/main/java/com/twu/biblioteca/Authenticator.java
@@ -2,15 +2,42 @@ package com.twu.biblioteca;
 
 import java.util.HashMap;
 
+import static com.twu.biblioteca.DummyUser.*;
+
 public class Authenticator {
     private static HashMap<Integer, String> userCredentials;
+    private static HashMap<Integer, User> userAccounts;   //LIBRARY ID - USER
 
     static {
+        userAccounts = new HashMap<>();
         userCredentials = new HashMap<>();
-        userCredentials.put(DummyUser.libraryNumber, DummyUser.password);
+        User user = new User(name, email, phoneNo);
+        userCredentials.put(libraryNumber, password);
+        userAccounts.put(libraryNumber, user);
+    }
+
+    private AppInteraction appInteraction;
+
+    Authenticator(AppInteraction appInteraction) {
+        this.appInteraction = appInteraction;
     }
 
     public boolean isValidUser(Integer userId, String password) {
-        return userCredentials.containsKey(userId) && userCredentials.get(userId).equals(password);
+        if (!userCredentials.containsKey(userId) || !userCredentials.get(userId).equals(password)) return false;
+        printUserInformation(userId); //TODO - violates SRP?
+        return true;
+    }
+
+    private void printUserInformation(int userId) {
+        User user = getUser(userId);
+
+        appInteraction.
+                print("Name : " + user.getName() +
+                        "\nEmail : " + user.getEmail() +
+                        "\nPhone Number : " + user.getPhoneNumber());
+    }
+
+    private User getUser(int userId) {
+        return userAccounts.get(userId);
     }
 }
--- src/test/java/com/twu/biblioteca/AuthenticatorTest.java
+++ src/test/java/com/twu/biblioteca/AuthenticatorTest.java
@@ -5,13 +5,16 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.*;
 
 class AuthenticatorTest {
+    AppInteraction appInteraction;
     private Authenticator authenticator;
 
     @BeforeEach
     public void initialize() {
-        authenticator = new Authenticator();
+        appInteraction = mock(AppInteraction.class);
+        authenticator = new Authenticator(appInteraction);
     }
 
     @Test
@@ -27,6 +30,20 @@ class AuthenticatorTest {
     @Test
     public void testReturnsFalseIfItGetsInvalidUserId() {
         assertFalse(authenticator.isValidUser(DummyUser.invalidLibraryNumber, DummyUser.password));
+    }
+
+    @Test
+    public void testPrintsUserDetailsIfItsAvalidAccount() {
+        authenticator.isValidUser(DummyUser.libraryNumber, DummyUser.password);
+
+        verify(appInteraction, times(1)).print(DummyUser.information);
+    }
+
+    @Test
+    public void testShouldNotPrintUserDetailsIfItsAvalidAccount() {
+        authenticator.isValidUser(DummyUser.invalidLibraryNumber, DummyUser.password);
+
+        verify(appInteraction, times(0)).print(DummyUser.information);
     }
 
 }
