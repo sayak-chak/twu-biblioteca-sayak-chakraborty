586bae2e850a7d09eb4aa7ba1020e3cd6862f24d
Sayak Chakraborty <sayak.chakraborty@thoughtworks.com>
2020-02-12 20:09:03 +0530

    Requirement 1.3 | Fix | Change name of menuOptions in Menu class to bookMenuOptions

--- src/main/java/com/twu/biblioteca/Menu.java
+++ src/main/java/com/twu/biblioteca/Menu.java
@@ -7,10 +7,10 @@ import static java.util.Arrays.asList;
 
 public final class Menu {
     public final static int choiceDisplayBooks = 1, choiceCheckout = 2, choiceReturn = 3, choiceQuit = 4;
-    public static List<String> menuOptions = asList(menuOptionOne, menuOptionTwo, menuOptionThree, menuOptionFour);
+    public static List<String> bookMenuOptions = asList(menuOptionOne, menuOptionTwo, menuOptionThree, menuOptionFour);
 
     public static void display() {
-        new AppInteraction(new InputOutputStream()).printList(menuOptions);
+        new AppInteraction(new InputOutputStream()).printList(bookMenuOptions);
     }
 
 }
